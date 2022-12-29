dd95c86de4d7567fad7257093b349e0d2639bde3
Anietie Lawrence Hanson Essien <arrhnnie@gmail.com>
2022-12-29 01:39:05 +0100

    Add files via upload

---
+++ While_And_Continue.py
@@ -0,0 +1,14 @@
+print("Launch the rocket in...\n")
+i = 10
+print(i)
+while i <= 10:
+    i = i - 1
+    if i == 5:
+        print("Blurp... Sorry")
+        continue
+    if i == 0:
+        print("Launching...")
+        break
+    print(i)
+    
+print("\n\n      ..\n     ....\n    ......\n   ........\n  ..........\n ............\n..............\n.            .\n.   |\   |   .\n.   | \  |   .\n.   |  \ |   .\n.   |   \|   .\n.            .\n.     /\     .\n.    /  \    .\n.   /----\   .\n.  /      \  .\n.    ____    .\n.   /        .\n.   \____    .\n.        \   .\n.   _____/   .\n.            .\n.     /\     .\n.    /  \    .\n.   /----\   .\n.  /      \  .\n.            .\n..............\n  /\  /\  /\ \n /__\/__\/__\ \n nnnnnnnnnnnn \nnnnnnnnnnnnnnn \nnnnnnnnnnnnnnn \nnnnnnnnnnnnnnn \n nnn nnnn nnn \n  n   nn   n \n\n\n\nOops...\n\n\nHouston we have a problem...")
